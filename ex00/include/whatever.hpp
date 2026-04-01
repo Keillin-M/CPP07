add2821df1355a02df009e220a57c106e1fda011
Keillin-M <keillin.maeda@gmail.com>
2026-04-01 15:37:36 +0200

    ex00 finished

--- ex00/include/whatever.hpp
+++ ex00/include/whatever.hpp
@@ -6,8 +6,20 @@
 /*   By: kmaeda <kmaeda@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 13:45:43 by kmaeda            #+#    #+#             */
-/*   Updated: 2026/02/23 13:45:44 by kmaeda           ###   ########.fr       */
+/*   Updated: 2026/04/01 15:30:12 by kmaeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+template <typename T> void swap(T& x, T& y) {
+	T temp = x;
+	x = y;
+	y = temp;
+};
 
+template <typename T> T max(const T& x, const T& y) {
+	return (x > y ? x : y);
+}
+
+template <typename T> T min(const T& x,const T& y) {
+	return (x < y ? x : y);
+}
---
+++ ex00/src/main.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmaeda <kmaeda@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 13:46:00 by kmaeda            #+#    #+#             */
+/*   Updated: 2026/04/01 15:36:41 by kmaeda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/whatever.hpp"
+#include <iostream>
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+	std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
+
+/*int main() {
+    std::cout << "=== Test 1: Integers ===" << std::endl;
+    int a = 5, b = 10;
+    std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+    ::swap(a, b);
+    std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
+    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+    std::cout << std::endl << "=== Test 2: Doubles ===" << std::endl;
+    double x = 3.14, y = 2.71;
+    std::cout << "Before swap: x = " << x << ", y = " << y << std::endl;
+    ::swap(x, y);
+    std::cout << "After swap: x = " << x << ", y = " << y << std::endl;
+    std::cout << "min(x, y) = " << ::min(x, y) << std::endl;
+    std::cout << "max(x, y) = " << ::max(x, y) << std::endl;
+
+    std::cout << std::endl << "=== Test 3: Strings ===" << std::endl;
+    std::string s1 = "apple", s2 = "banana";
+    std::cout << "Before swap: s1 = " << s1 << ", s2 = " << s2 << std::endl;
+    ::swap(s1, s2);
+    std::cout << "After swap: s1 = " << s1 << ", s2 = " << s2 << std::endl;
+    std::cout << "min(s1, s2) = " << ::min(s1, s2) << std::endl;
+    std::cout << "max(s1, s2) = " << ::max(s1, s2) << std::endl;
+
+    std::cout << std::endl << "=== Test 4: Equal values ===" << std::endl;
+    int e1 = 42, e2 = 42;
+    std::cout << "min(e1, e2) = " << ::min(e1, e2) << std::endl;
+    std::cout << "max(e1, e2) = " << ::max(e1, e2) << std::endl;
+
+    return 0;
+}*/
